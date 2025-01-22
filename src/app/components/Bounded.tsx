127900020b54322fc878e8f03baf57019e5fcc1a
ayush <chhotemotelogkda@gmail.com>
2025-01-22 22:56:48 +0530

    Final

--- src/app/components/Bounded.tsx
+++ src/app/components/Bounded.tsx
@@ -1,24 +1,24 @@
-import { ElementType, ReactNode, ComponentPropsWithoutRef } from "react";
+import { CSSProperties, ElementType, ReactNode } from "react";
 import clsx from "clsx";
 
-type BoundedProps<T extends ElementType> = {
-  as?: T; // The element/component type
-  className?: string; // Additional class names
-  style?: React.CSSProperties; // Optional inline styles
-  children: ReactNode; // Allow children (single/multiple)
-} & Omit<ComponentPropsWithoutRef<T>, "as" | "className" | "children">; // Exclude conflicting props
+type BoundedProps = {
+  as?: ElementType;
+  className?: never;
+  style?: CSSProperties;
+  children: ReactNode;
+};
 
-export function Bounded<T extends ElementType = "section">({
-  as: Comp = "section", // Default to "section" if no "as" provided
+export function Bounded({
+  as: Comp = "section",
   className,
   children,
   ...restProps
-}: BoundedProps<T>) {
+}: BoundedProps) {
   return (
     <Comp
       className={clsx(
         "px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32",
-        className
+        className,
       )}
       {...restProps}
     >
